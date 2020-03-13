7a928c184bb61fca0d4630ac04fe3d005265d4e7
Ximoromero5 <chimillo_95@hotmail.com>
2020-03-13 14:02:51 +0100

    last

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDo6k6hWSvLAiIdy6uSVOxFrmVqigCWkYk",
+    authDomain: "fir-app-a9dcf.firebaseapp.com",
+    databaseURL: "https://fir-app-a9dcf.firebaseio.com",
+    projectId: "fir-app-a9dcf",
+    storageBucket: "fir-app-a9dcf.appspot.com",
+    messagingSenderId: "1090939122577",
+    appId: "1:1090939122577:web:ad6a7ec32b5094c2afa030",
+    measurementId: "G-ZNJCKP8Y4P"
+  }
 };
